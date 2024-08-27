f7a7f115233dd08b131400dbf408846522678db3
subhajit-007 <subhajit.mandal@nagarro.com>
2024-08-27 20:56:56 +0530

    Update dashboard

--- appointment-scheduling-fe/src/app/screens/patient/dashboard/dashboard.component.ts
+++ appointment-scheduling-fe/src/app/screens/patient/dashboard/dashboard.component.ts
@@ -2,40 +2,45 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatLabel } from '@angular/material/form-field';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, Router } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { DoctorService } from '../../../services/doctor/doctor.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 
 const MatModules = [
   MatToolbarModule,
   MatLabel,
-  // MatFormFieldModule,
-  // MatInputModule,
+  MatFormFieldModule,
+  MatInputModule,
   MatButtonModule,
-  // MatIconModule,
+  MatIconModule,
   MatListModule,
   MatDividerModule,
 ];
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [...MatModules, RouterModule, CommonModule],
+  imports: [...MatModules, RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   doctorsNameList: any[] = [];
 
+  searchText = new FormControl('')
+
   constructor(private doctorService: DoctorService, private router: Router) { }
 
   ngOnInit(): void {
     this.doctorService.searchDoctors().subscribe({
       next: (res) => {
-        console.log(res.data);
+        // console.log(res.data);
         this.doctorsNameList = res.data;
       },
       error: (err) => {
@@ -46,5 +51,19 @@ export class DashboardComponent {
 
   goToDoctorDetailPage(doctorId: any): void {
     this.router.navigate([`/doctor/${doctorId}/details`])
+  }
+
+  searchDoctors(): void {
+    console.log(this.searchText.value)
+    const searchQuery = `specialty=${this.searchText.value}`
+    this.doctorService.searchDoctors(searchQuery).subscribe({
+      next: (res) => {
+        // console.log(res.data);
+        this.doctorsNameList = res.data;
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
   }
 }
